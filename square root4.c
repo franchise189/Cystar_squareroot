ad8311de738fae59058ac7abffd8c915426e5c5e
franchise189 <83475511+franchise189@users.noreply.github.com>
2022-10-06 15:17:42 +0300

    Create square root4.c

---
+++ square root4.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+ 
+
+double findSQRT(double Number) { return sqrt(Number); }
+ 
+
+int main()
+{
+ 
+    
+    int Number;
+    
+    scanf("%d",Enter Number);
+ 
+    printf("%f ", findSQRT(Number));
+    
+    return 0;
+}
