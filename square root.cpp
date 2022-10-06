0ee9d2ae15a1531ed3099c03abce6902f07de933
franchise189 <83475511+franchise189@users.noreply.github.com>
2022-10-06 13:20:42 +0300

    Create square root.cpp

---
+++ square root.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+{
+int main()  
+int number;
+float squareroot;
+cout<<"Enter number:";
+cin>>number;
+squareroot=sqrt(number);
+cout<<"squareroot is:";
+cin<<squareroot;
+return 0;
+}
