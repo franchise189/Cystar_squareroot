2440da1eee32c62c655c6dc9f9a621e24403b3b0
franchise189 <83475511+franchise189@users.noreply.github.com>
2022-10-06 13:48:07 +0300

    Create square root2.cpp

---
+++ square root2.cpp
@@ -0,0 +1,24 @@
+
+#include<math.h>
+
+using namespace std;
+
+
+{
+    int main()
+    int number;
+
+    float squreroot;
+
+    cout<<"Enter number:";
+
+    cin>>number;
+
+    squreroot=number*number;
+
+    cout<<"squreroot is:";
+
+    cin>>squreroot;
+
+    return 0;
+}
